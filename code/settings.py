e01b5d78f45a789fe21df7123f298a23a0164991
RyanNoelk <ryannoelk@gmail.com>
2016-08-22 11:21:05 +0200

    different podcast db name

--- code/settings.py
+++ code/settings.py
@@ -6,7 +6,7 @@ import os
 
 FROM_EMAIL_ADDRESS = "ichbinsub@gmail.com"
 CURRENT_DIRECTORY = os.path.realpath(os.path.dirname(sys.argv[0]))
-DB_NAME = "PodGrab.db"
+DB_NAME = "podcast.db"
 #DOWNLOAD_DIRECTORY = "/data/ryannoelk/files/podcasts"
 DOWNLOAD_DIRECTORY = "/Users/ryannoelk/code/podcast-downloader/podcasts"
 NUMBER_OF_PODCASTS_TO_KEEP = 2
