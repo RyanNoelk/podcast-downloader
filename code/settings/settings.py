f2b2c0db2d3eb2e47a6e9bf2005d85e3d056d817
RyanNoelk <ryannoelk@gmail.com>
2016-08-22 11:29:33 +0200

    updaing settings

--- code/settings/settings.py
+++ code/settings/settings.py
@@ -5,8 +5,13 @@ import sys
 import os
 
 FROM_EMAIL_ADDRESS = "ichbinsub@gmail.com"
+EMAIL_PASSWORD = "abcdefg"
 CURRENT_DIRECTORY = os.path.realpath(os.path.dirname(sys.argv[0]))
+DOWNLOAD_DIRECTORY = CURRENT_DIRECTORY + os.sep + "podcasts"
 DB_NAME = "podcast.db"
-#DOWNLOAD_DIRECTORY = "/data/ryannoelk/files/podcasts"
-DOWNLOAD_DIRECTORY = "/Users/ryannoelk/code/podcast-downloader/podcasts"
 NUMBER_OF_PODCASTS_TO_KEEP = 2
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
